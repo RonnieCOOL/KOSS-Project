3582c1f2ce9715ee9eafeb04a587b8d8b3eafb5a
RonnieCOOL <98256869+RonnieCOOL@users.noreply.github.com>
2022-06-09 23:32:04 +0530

    Added file

---
+++ Task 1/Downloading Page.py
@@ -0,0 +1,29 @@
+import asyncio
+import aiohttp
+import time
+
+
+async def download(url):
+    async with aiohttp.ClientSession() as session:
+        response = await(session.get(url))
+    return response
+
+
+async def main():
+    arr = [1, 2, 3]
+    url = 'https://reqres.in/api/users?page{}'
+    tasks = []
+    for el in arr:
+        tasks.append(download(url.format(el)))
+
+    start = time.time()
+    await asyncio.gather(*tasks);
+
+    time_taken = time.time() - start
+    print('Time Taken {0}'.format(time_taken))
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(main())
+# Completed in 0.18 sec (Average)
+# Best - 0.14 sec
+# Worst - 0.26 sec
