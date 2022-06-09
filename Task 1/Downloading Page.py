7c7dab79668af7c8b455a87db937bbb5851eb291
RonnieCOOL <98256869+RonnieCOOL@users.noreply.github.com>
2022-06-10 00:12:34 +0530

    Update Downloading Page.py

--- Task 1/Downloading Page.py
+++ Task 1/Downloading Page.py
@@ -10,15 +10,14 @@ async def download(url):
 
 
 async def main():
-    start = time.time()
     arr = [1, 2, 3]
     url = 'https://reqres.in/api/users?page{}'
     tasks = []
     for el in arr:
         tasks.append(download(url.format(el)))
 
-#     await asyncio.gather(*tasks);
-
+    start = time.time()
+    await asyncio.gather(*tasks);
     time_taken = time.time() - start
     print('Time Taken {0}'.format(time_taken))
 
